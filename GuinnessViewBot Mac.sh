a0f04e355c888ef36ec5f6196fcd87a5ad221ee1
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-11-15 23:05:39 -0800

    Update GuinnessViewBot Mac.sh

--- GuinnessViewBot Mac.sh
+++ GuinnessViewBot Mac.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew not found. Installing Homebrew..."
+which -s brew
+if [[ $? != 0 ]] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-if ! command -v python3 &> /dev/null
-then
-    echo "Python not found. Installing Python..."
-    brew install python3
-fi
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+brew install python
 source ~/.bash_profile
+export PATH="/usr/local/bin:$PATH"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install requests pystyle urllib3
 cd "$(dirname "$0")/data"
-python3 GuinnessViewBot.py
+python GuinnessViewBot.py
